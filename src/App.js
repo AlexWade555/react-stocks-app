8a0a3b2316592a26d7f2381c09e405e7ff72a44e
Alex Wade <alexjohnwade@gmail.com>
2022-11-29 12:56:38 +1100

    added pages and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css';
 
 export default function App() {
   return (
     <main>
-      <h2 className="mt-5 p5 bg-primary">Hello</h2>
+      <BrowserRouter>
+        <Routes>
+          <Route>
+
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </main>
   );
 }
