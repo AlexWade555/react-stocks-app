b26f4d9252ad1c534a04dc1d473cb3ba617b7c21
Alex Wade <alexjohnwade@gmail.com>
2022-11-29 13:19:05 +1100

    added usestte and useefect to stocklist

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -1,3 +1,6 @@
+import { useState, useEffect } from "react"
+
 export const StockList = () => {
+  const [watchlist, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"])
   return <div>StockList</div>
 }
