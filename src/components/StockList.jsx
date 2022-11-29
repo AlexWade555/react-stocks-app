6188113b6e93db90e47801e0c7fabddaaf1e6a1c
Alex Wade <alexjohnwade@gmail.com>
2022-11-29 14:48:15 +1100

    exit'

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -3,24 +3,18 @@ import finnHub from "../apis/finnHub"
 
 export const StockList = () => {
   const [stock, setStock] = useState()
-  const [watchlist, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"])
+  const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"])
 
   useEffect(() => {
     let isMounted = true
     const fetchData = async () => {
       try {
-        const responses = Promise.all(finnHub.get("/quote", {
-          params: {
-            symbol: "GOOGL"
-          }
-        }), finnHub.get("/quote", {
-          params: {
-            symbol: "MSFT"
-          }
-        }), finnHub.get("/quote", {
-          params: {
-            symbol: "AMZN"
-          }
+        const responses = await Promise.all(watchList.map((stock) => {
+          return finnHub.get("/quote", {
+            params: {
+              symbol: stock
+            }
+          })
         }))
         console.log(responses)
         if (isMounted) {
