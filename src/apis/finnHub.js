1267ac5d7d5b9b546f2c28aaee713903dcb01022
Alex Wade <alexjohnwade@gmail.com>
2022-11-29 13:41:05 +1100

    added token to finhub.js

--- src/apis/finnHub.js
+++ src/apis/finnHub.js
@@ -1,5 +1,11 @@
 import axios from "axios";
+import { useParams } from "react-router-dom";
+
+const TOKEN = "cduq4oaad3i2h5f4nrfgcduq4oaad3i2h5f4nrg0"
 
 export default axios.create({
-  baseURL: "https://finnhub.io/api/v1"
+  baseURL: "https://finnhub.io/api/v1",
+  params: {
+    token: TOKEN
+  }
 })
--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -1,6 +1,20 @@
 import { useState, useEffect } from "react"
+import finnHub from "../apis/finnHub"
 
 export const StockList = () => {
   const [watchlist, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await finnHub.get("/quote?symbol=MSFT&token=cduq4oaad3i2h5f4nrfgcduq4oaad3i2h5f4nrg0")
+        console.log(response)
+      } catch (error) {
+
+      }
+    }
+    fetchData()
+  }, [])
+
   return <div>StockList</div>
 }
