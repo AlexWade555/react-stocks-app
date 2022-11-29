9ff327293c5f1016c19697d1156ef64d6d059762
Alex Wade <alexjohnwade@gmail.com>
2022-11-29 14:29:06 +1100

    added promise.all for each stock in list

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -2,22 +2,37 @@ import { useState, useEffect } from "react"
 import finnHub from "../apis/finnHub"
 
 export const StockList = () => {
+  const [stock, setStock] = useState()
   const [watchlist, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"])
 
   useEffect(() => {
+    let isMounted = true
     const fetchData = async () => {
       try {
-        const response = await finnHub.get("/quote", {
+        const responses = Promise.all(finnHub.get("/quote", {
+          params: {
+            symbol: "GOOGL"
+          }
+        }), finnHub.get("/quote", {
           params: {
             symbol: "MSFT"
           }
-        })
-        console.log(response)
+        }), finnHub.get("/quote", {
+          params: {
+            symbol: "AMZN"
+          }
+        }))
+        console.log(responses)
+        if (isMounted) {
+          setStock(responses.data)
+        }
       } catch (error) {
 
       }
     }
     fetchData()
+
+    return () => (isMounted = false)
   }, [])
 
   return <div>StockList</div>
