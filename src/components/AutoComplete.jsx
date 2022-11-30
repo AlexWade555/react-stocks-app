a784d68e8e0132e4bb979228a90ce509d9d7ac2e
Alex Wade <alexjohnwade@gmail.com>
2022-11-30 14:05:08 +1100

    added in line styling for dropdown menu

--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -8,10 +8,15 @@ export const AutoComplete = () => {
   const renderDropdown = () => {
     const dropDownClass = search ? "show" : null
     return (
-      <ul className={`dropdown-menu ${dropDownClass}`}>
+      <ul style={{
+        height: "500px",
+        overflowY: "scroll",
+        overflowX: "hidden",
+        cursor: "pointer"
+      }}className={`dropdown-menu ${dropDownClass}`}>
         {results.map((result) => {
           return (
-            <li className="dropdown-item">{result.description}({result.symbol})</li>
+            <li key={result.symbol} className="dropdown-item">{result.description}({result.symbol})</li>
           )
         })}
       </ul>
