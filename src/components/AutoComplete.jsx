1c168132820e040b4dd2c7b92a3c2b4a99fa937d
Alex Wade <alexjohnwade@gmail.com>
2022-11-30 11:49:47 +1100

    added useStae for search

--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -1,10 +1,18 @@
+import { useState, useEffect } from "react";
+
 export const AutoComplete = () => {
+  const [search, setSearch] = useState("")
+
+  useEffect(() => {
+
+  })
+
   return <div className="w-50 p-5 rounded mx-auto">
     <div className="form-floating dropdown">
-      <input style={{backgroundColor: "rgba(145, 158, 171, 0.04"}} id="search" type="text" className="form-control" placeholder="Search" autoComplete="off"></input>
+      <input style={{backgroundColor: "rgba(145, 158, 171, 0.04"}} id="search" type="text" className="form-control" placeholder="Search" autoComplete="off" value={search} onChange={(e) => setSearch(e.target.value)}></input>
       <label htmlFor="search">Search</label>
     </div>
-    <ul>
+    <ul className="dropdown-menu">
       <li>stock1</li>
       <li>stock2</li>
       <li>stock3</li>
