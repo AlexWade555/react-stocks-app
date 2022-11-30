e65b3d36756024eb3d26853dfbb23572c04951f9
Alex Wade <alexjohnwade@gmail.com>
2022-11-30 11:37:54 +1100

    added drop down for search

--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -1,7 +1,13 @@
 export const AutoComplete = () => {
   return <div className="w-50 p-5 rounded mx-auto">
     <div className="form-floating dropdown">
-      <input style={{backgroundColor: "rgba(145, 158, 171, 0.04"}} id="search" type="text" className="form-control" placeholder="Search" ></input>
+      <input style={{backgroundColor: "rgba(145, 158, 171, 0.04"}} id="search" type="text" className="form-control" placeholder="Search" autoComplete="off"></input>
+      <label htmlFor="search">Search</label>
     </div>
+    <ul>
+      <li>stock1</li>
+      <li>stock2</li>
+      <li>stock3</li>
+    </ul>
   </div>
 }
