4b83dcf4c0473ea870f8389425dbb4c0ec148c64
Alex Wade <alexjohnwade@gmail.com>
2022-11-30 14:41:43 +1100

    added watch list to conext provider

--- src/context/watchListContext.jsx
+++ src/context/watchListContext.jsx
@@ -1,9 +1,12 @@
 import { createContext } from "react";
+import { useState } from "react";
 
 export const WatchListContext = createContext()
 
 export const WatchListContextProvider = (props) => {
-  return <WatchListContext.Provider>
 
+  const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"])
+
+  return <WatchListContext.Provider value={{watchList}}>
   </WatchListContext.Provider>
 }
