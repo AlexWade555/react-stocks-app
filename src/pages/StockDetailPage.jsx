5b9c30cc75ec0413e7f413cf92bbe7fd9b3a5ad3
Alex Wade <alexjohnwade@gmail.com>
2022-12-05 13:37:45 +1100

    added params and maths to figure out time for detail page

--- src/pages/StockDetailPage.jsx
+++ src/pages/StockDetailPage.jsx
@@ -6,8 +6,18 @@ import finnHub from "../apis/finnHub"
 export const StockDetailPage = () => {
   const {symbol} = useParams()
   useEffect(() => {
-    const fetchData = () => {
-
+    const fetchData = async () => {
+      const date = new Date()
+      const currentTime = Math.floor(date.getTime()/1000)
+      const oneDay = currentTime - 24*60*60
+      const response = await finnHub.get("/stock/candle", {
+        params: {
+          symbol,
+          from: oneDay,
+          to: currentTime,
+          resolution: 30
+        }
+      })
     }
   })
 
