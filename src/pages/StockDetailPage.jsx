e9d204a92c5c6050b89ee10d194f37f4c9bf546b
Alex Wade <alexjohnwade@gmail.com>
2022-12-05 13:29:57 +1100

    imported relvant functions and fetchdata for details page

--- src/pages/StockDetailPage.jsx
+++ src/pages/StockDetailPage.jsx
@@ -1,3 +1,15 @@
+import { useParams } from "react-router-dom"
+import { useEffect } from "react"
+import axios from "axios"
+import finnHub from "../apis/finnHub"
+
 export const StockDetailPage = () => {
-  return <div>StockDetailPage</div>
+  const {symbol} = useParams()
+  useEffect(() => {
+    const fetchData = () => {
+
+    }
+  })
+
+  return <div>StockDetailPage {symbol}</div>
 }
