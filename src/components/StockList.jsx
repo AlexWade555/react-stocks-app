0101d07c1dfd21323d901a5fcbae883976c0c8d5
Alex Wade <alexjohnwade@gmail.com>
2022-12-05 13:15:23 +1100

    added routes to detail page

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useContext } from "react"
+import { useNavigate } from "react-router-dom"
 import {BsFillCaretUpFill, BsFillCaretDownFill} from "react-icons/bs"
 import { WatchListContext } from "../context/watchListContext"
 
@@ -7,7 +8,7 @@ import finnHub from "../apis/finnHub"
 export const StockList = () => {
   const [stock, setStock] = useState([])
   const { watchList } = useContext(WatchListContext)
-  // const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"])
+  const navigate = useNavigate()
 
   const changeColor = (change) => {
     return change > 0 ? "success" : "danger"
@@ -48,6 +49,10 @@ export const StockList = () => {
     return () => (isMounted = false)
   }, [watchList])
 
+  const handleStockSelect = (symbol) => {
+    navigate(`detail/${symbol}`)
+  }
+
   return <div>
     <table className="table hover mt-5">
       <thead style={{color: "rgb(79,89,102)"}}>
@@ -65,7 +70,7 @@ export const StockList = () => {
       <tbody>
         {stock.map((stockData) => {
           return (
-            <tr className="table-row" key={stockData.symbol}>
+            <tr onClick={() => handleStockSelect(stockData.symbol)} className="table-row" key={stockData.symbol}>
               <th scope="row">{stockData.symbol}</th>
               <td>{stockData.data.c}</td>
               <td className={`text-${changeColor(stockData.data.d)}`}>{stockData.data.d} {renderIcon(stockData.data.d)}</td>
