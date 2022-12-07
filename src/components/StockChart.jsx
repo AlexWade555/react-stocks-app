b87b69587421aaf9e00950bb05fb0b165c0fde93
Alex Wade <alexjohnwade@gmail.com>
2022-12-07 13:28:59 +1100

    added btns for time periods

--- src/components/StockChart.jsx
+++ src/components/StockChart.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react"
 import Chart from "react-apexcharts"
 
 export const StockChart = ({chartData, symbol}) => {
+  const [dateFormat, setDateFormat] = useState("24h")
   const {day, week, year} = chartData
   const options = {
     title: {
@@ -35,5 +37,10 @@ export const StockChart = ({chartData, symbol}) => {
 
   return <div className= "mt-5 p-4 shadow-sm bg-white">
     <Chart options={options} series={series} type="area" width="100%"/>
+    <div className="">
+      <button onClick={() => setDateFormat("24h")}>24h</button>
+      <button onClick={() => setDateFormat("7d")}>7d</button>
+      <button onClick={() => setDateFormat("1y")}>1y</button>
+    </div>
   </div>
 }
