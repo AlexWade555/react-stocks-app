40b03cc3f20a10106bd96c113052803c84d34187
Alex Wade <alexjohnwade@gmail.com>
2022-12-07 15:19:27 +1100

    added the stockdata to usestate

--- src/components/StockData.jsx
+++ src/components/StockData.jsx
@@ -2,7 +2,10 @@ import { useState, useEffect } from "react"
 import finnHub from "../apis/finnHub"
 
 export const StockData = ({symbol}) => {
+
+  const [stockData, setStockData] = useState()
   useEffect(() => {
+    let isMounted = true;
     const fetchData = async () => {
       try {
         const response = await finnHub.get("/stock/profile2", {
@@ -11,6 +14,9 @@ export const StockData = ({symbol}) => {
           }
         })
         console.log(response)
+        if (isMounted) {
+          setStockData(response.data)
+        }
       } catch (error) {
 
       }
