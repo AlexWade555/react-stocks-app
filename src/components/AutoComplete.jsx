742611ba59eb2fff50142894babdc7ad92b94692
Alex Wade <alexjohnwade@gmail.com>
2023-01-23 17:20:07 +1100

    added css

--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -57,7 +57,9 @@ export const AutoComplete = () => {
   }, [search])
 
   return <div className="w-50 p-5 rounded mx-auto">
+    <div className="d-flex justify-content-center">
     <Logo/>
+    </div>
     <div className="form-floating dropdown">
       <input style={{backgroundColor: "rgba(145, 158, 171, 0.04"}} id="search" type="text" className="form-control" placeholder="Search" autoComplete="off" value={search} onChange={(e) => setSearch(e.target.value)}></input>
       <label htmlFor="search">Search</label>
