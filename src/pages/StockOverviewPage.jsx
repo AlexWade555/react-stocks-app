76a0de2066cfe838d18fb59eeddb05b5ae7540c9
Alex Wade <alexjohnwade@gmail.com>
2023-01-23 17:04:55 +1100

    trying to add logo

--- src/pages/StockOverviewPage.jsx
+++ src/pages/StockOverviewPage.jsx
@@ -1,8 +1,10 @@
 import {AutoComplete} from '../components/AutoComplete'
 import { StockList } from '../components/StockList'
+import logo from '../Logo.svg'
 
 export const StockOverviewPage = () => {
   return <div>
+    <img scr={logo} alt="logo"/>
     <AutoComplete/>
     <StockList/>
   </div>
